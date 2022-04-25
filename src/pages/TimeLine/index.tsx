ddb26be828405a7ea41c5c58c568da6ba51e7f3a
guoguokakukaku <guoguokakukaku@outlook.com>
2022-04-26 00:48:40 +0900

    軽微修正

--- src/pages/TimeLine/index.tsx
+++ src/pages/TimeLine/index.tsx
@@ -46,13 +46,13 @@ function TimeLine() {
             <a href='#' className='title2' onClick={showModal}>
               ekyc本人オンライン確認サービス
             </a>
-            <p className='title3'>
+            <div className='title3'>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
-            </p>
+            </div>
             <p>
               スマートフォン上で撮影した本人確認書類と顔写真だけで本人確認が可能になるサービスである。
               サーバー側はnodeJsで実装され、クライアント側はreactを利用している。私はクライアントに参加している。クライアント側は書類識別SDKを使って、スマートフォンのカメラで本人確認用の書類を自動識別機能を実装して様々な書類で本人確認をできる機能を実現できた。
@@ -64,13 +64,13 @@ function TimeLine() {
             <a href='#' className='title2' onClick={showModal}>
               文書管理システム
             </a>
-            <p className='title3'>
+            <div className='title3'>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
-            </p>
+            </div>
             <p>
               現場を開設してから竣工までの文書を管理するシステムとなる。 我々のチームはサーバ側の開発を担当していて
               私は具体的にフォルダの作成・移動および削除などの機能を担当していた。
@@ -82,13 +82,13 @@ function TimeLine() {
             <a href='#' className='title2' onClick={showModal}>
               ekyc本人オンライン確認サービス
             </a>
-            <p className='title3'>
+            <div className='title3'>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
               <div className='item'>React</div>
-            </p>
+            </div>
             <p>
               スマートフォン上で撮影した本人確認書類と顔写真だけで本人確認が可能になるサービスである。
               サーバー側はnodeJsで実装され、クライアント側はreactを利用している。私はクライアントに参加している。クライアント側は書類識別SDKを使って、スマートフォンのカメラで本人確認用の書類を自動識別機能を実装して様々な書類で本人確認をできる機能を実現できた。
